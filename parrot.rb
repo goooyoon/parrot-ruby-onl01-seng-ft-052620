9e1f780cbb4eba21ef6281ffff3e13fc0a7d14e1
Grace Kim <goooyoon@gmail.com>
2020-05-26 21:18:28 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(phrase)
-  return "Squawk!"
-  puts "Pretty bird!"
+def parrot(string="Squawk!")
+  puts "#{string}"
+  "#{string}"
 end
